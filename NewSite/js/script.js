cb6e0b5f1ac53c78b44fe2593fca1a0d6a14d74b
Sitarnet <sitarnet@ukr.net>
2020-07-16 18:01:49 +0300

    Video, slider

---
+++ NewSite/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 3,
+        //autoplay: true,
+        // autoplaySpeed: 3000,
+    });
+    $('.popup').fancybox()
+
+});
